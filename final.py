dadc2c8e2563db2f1da8865913745336554fe5a4
ArthurDF <32194699+ArthurDF@users.noreply.github.com>
2023-03-15 11:23:45 -0300

    Update final.py

--- final.py
+++ final.py
@@ -13,7 +13,7 @@ import spatialpandas as spd
 import geopandas as gpd
 import rioxarray as rxr
 import numpy as np
-
+import hvplot.pandas
 
 hv.extension('bokeh')
 pn.extension(sizing_mode = 'stretch_width')
@@ -53,7 +53,7 @@ class AppTest(param.Parameterized):
     gdf_parques = gpd.read_file(parques_add)
     gdf_parques = gdf_parques.to_crs(3857)
     spd_parques = spd.GeoDataFrame(gdf_parques)
-    #spd_parques_plot = spd_parques.hvplot('green',responsive=True)
+    spd_parques_plot = spd_parques.hvplot('green',responsive=True)
     
     gdf_rest = gpd.read_file(add_rest)
     gdf_rest = gdf_rest.to_crs(3857)
